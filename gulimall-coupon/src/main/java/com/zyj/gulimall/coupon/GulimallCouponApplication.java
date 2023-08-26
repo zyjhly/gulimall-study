c4c14fc402b0ee769ef2e5e20be2e37da67f85b9
zyjhly <1677706003@qq.com>
2023-08-26 14:17:17 +0800

    基础环境搭建完成

--- gulimall-coupon/src/main/java/com/zyj/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/zyj/gulimall/coupon/GulimallCouponApplication.java
@@ -1,8 +1,10 @@
 package com.zyj.gulimall.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.zyj.gulimall.coupon.dao")
 @SpringBootApplication
 public class GulimallCouponApplication {
 
--- gulimall-product/src/main/java/com/zyj/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/zyj/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,9 @@
 package com.zyj.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.zyj.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
